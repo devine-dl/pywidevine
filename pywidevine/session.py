999900278f9f3ab425c0b1527ddaca5fef7d731a
rlaphoenix <rlaphoenix@pm.me>
2022-07-30 04:31:03 +0100

    Create a Session class

---
+++ pywidevine/session.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from Crypto.Random import get_random_bytes
+
+from pywidevine.key import Key
+from pywidevine.license_protocol_pb2 import DrmCertificate
+
+
+class Session:
+    def __init__(self):
+        self.id = get_random_bytes(16)
+        self.service_certificate: Optional[DrmCertificate] = None
+        self.context: dict[bytes, tuple[bytes, bytes]] = {}
+        self.keys: list[Key] = []
