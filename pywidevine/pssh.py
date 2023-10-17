c71f867a72e16c7cec1fb97268ee433208865f60
mediaminister <45148099+mediaminister@users.noreply.github.com>
2023-10-17 21:40:47 +0200

    Use std-lib xml instead of lxml (#35)
    Allows for support on ARM devices and reduces dependencies.
    
    ---------
    
    Co-authored-by: rlaphoenix <rlaphoenix@pm.me>

--- pywidevine/pssh.py
+++ pywidevine/pssh.py
@@ -6,6 +6,7 @@ import string
 from io import BytesIO
 from typing import Optional, Union
 from uuid import UUID
+from xml.etree.ElementTree import XML
 
 import construct
 from construct import Container
@@ -13,7 +14,6 @@ from google.protobuf.message import DecodeError
 from pymp4.parser import Box
 
 from pywidevine.license_protocol_pb2 import WidevinePsshData
-from pywidevine.utils import load_xml
 
 
 class PSSH:
@@ -266,19 +266,30 @@ class PSSH:
                     # TODO: Add support for Embedded License Stores (0x03)
                     continue
 
-                prr_header = load_xml(prr_value.decode("utf-16-le"))
-                prr_header_version = prr_header.attrib["version"]
+                wrm_ns = {"wrm": "http://schemas.microsoft.com/DRM/2007/03/PlayReadyHeader"}
+                prr_header = XML(prr_value.decode("utf-16-le"))
+                prr_header_version = prr_header.get("version")
                 if prr_header_version == "4.0.0.0":
-                    key_ids = prr_header.xpath("DATA/KID/text()")
+                    key_ids = [
+                        x.text
+                        for x in prr_header.findall("./wrm:DATA/wrm:KID", wrm_ns)
+                    ]
                 elif prr_header_version == "4.1.0.0":
-                    key_ids = prr_header.xpath("DATA/PROTECTINFO/KID/@VALUE")
+                    key_ids = [
+                        x.get("VALUE")
+                        for x in prr_header.findall("./wrm:DATA/wrm:PROTECTINFO/wrm:KID", wrm_ns)
+                    ]
                 elif prr_header_version in ("4.2.0.0", "4.3.0.0"):
                     # TODO: Retain the Encryption Scheme information in v4.3.0.0
                     #       This is because some Key IDs can be AES-CTR while some are AES-CBC.
                     #       Conversion to WidevineCencHeader could use this information.
-                    key_ids = prr_header.xpath("DATA/PROTECTINFO/KIDS/KID/@VALUE")
+                    key_ids = [
+                        x.get("VALUE")
+                        for x in prr_header.findall("./wrm:DATA/wrm:PROTECTINFO/wrm:KIDS/wrm:KID", wrm_ns)
+                    ]
                 else:
                     raise ValueError(f"Unsupported PlayReadyHeader version {prr_header_version}")
+
                 return [
                     UUID(bytes=base64.b64decode(key_id))
                     for key_id in key_ids
